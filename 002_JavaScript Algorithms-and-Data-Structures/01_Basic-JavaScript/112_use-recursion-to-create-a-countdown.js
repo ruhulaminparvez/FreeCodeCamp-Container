b73d25cf5b408226cc07f39c2d6c2186f0f4fd96
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-02-17 15:47:09 +0600

    Use Recursion to Create a Countdown :octocat:

---
+++ 002_JavaScript Algorithms-and-Data-Structures/01_Basic-JavaScript/112_use-recursion-to-create-a-countdown.js
@@ -0,0 +1,14 @@
+// Only change code below this line
+function countdown(n) {
+  if (n < 1) {
+    return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+}
+// Only change code above this line
+console.log(countdown(-1));
+console.log(countdown(10));
+console.log(countdown(5));
