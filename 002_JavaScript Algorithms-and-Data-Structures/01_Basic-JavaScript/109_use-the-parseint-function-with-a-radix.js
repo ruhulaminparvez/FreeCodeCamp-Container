cdf39a9e189a6076d88dc1e37ac07f2930eb6727
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-02-17 15:22:34 +0600

    ParseInt() with a Radix :octocat:

---
+++ 002_JavaScript Algorithms-and-Data-Structures/01_Basic-JavaScript/109_use-the-parseint-function-with-a-radix.js
@@ -0,0 +1,10 @@
+function convertToInteger(str, radix) {
+
+  const a = parseInt(str, 2);
+  return a;
+}
+
+convertToInteger("10011");
+convertToInteger("10011");
+convertToInteger("111001");
+convertToInteger("JamesBond");
