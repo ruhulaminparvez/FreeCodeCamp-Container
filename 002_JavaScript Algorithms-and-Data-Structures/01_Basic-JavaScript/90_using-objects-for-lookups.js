a82f224066b4b47ea8411762e789bf8d3df0d27c
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-02-17 11:13:45 +0600

    Use Object for LooksUp :grey_question:

---
+++ 002_JavaScript Algorithms-and-Data-Structures/01_Basic-JavaScript/90_using-objects-for-lookups.js
@@ -0,0 +1,28 @@
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  };
+// After converting our case statements into object properties you can make use of the variable `result` to let the function return the correct value.
+
+
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+
+
+phoneticLookup("alpha");
+phoneticLookup("bravo");
+phoneticLookup("charlie");
+phoneticLookup("delta");
+phoneticLookup("echo");
+phoneticLookup("foxtrot");
+phoneticLookup("");
