b232fe1306d3819bc471829fc04f5e82e1f7c897
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-02-17 16:22:21 +0600

    Compare scopes of the var and let keywords :octocat:

---
+++ 002_JavaScript Algorithms-and-Data-Structures/02_ES6/01_compare-scopes-of-the-var-and-let-keywords.js
@@ -0,0 +1,11 @@
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (!true) {
+    i = "block scope";
+    console.log("Block scope i is: ", i);
+  } else {
+  console.log("Function scope i is: ", i);
+  return i;
+  }
+}
