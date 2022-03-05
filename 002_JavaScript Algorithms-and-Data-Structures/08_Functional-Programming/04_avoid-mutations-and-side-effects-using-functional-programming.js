adc290937276be27c1722074b05ceb6200bfd70a
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-03-05 11:24:50 +0600

    Avoid Mutations and Side Effects Using Functional Programming :octocat:

---
+++ 002_JavaScript Algorithms-and-Data-Structures/08_Functional-Programming/04_avoid-mutations-and-side-effects-using-functional-programming.js
@@ -0,0 +1,11 @@
+// The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+  return fixedValue + 1;
+  // Only change code above this line
+}
+
+var newValue = incrementer(); // Should equal 5
+console.log(fixedValue); // Should print 4
