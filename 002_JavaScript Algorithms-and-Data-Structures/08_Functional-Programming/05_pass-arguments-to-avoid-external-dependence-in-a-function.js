174b188adfb38b9604d236cf67ed25b95b2fc8bd
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-03-05 11:29:29 +0600

    Pass Argumenta to Avoid External Dependence in a Function :octocat:

---
+++ 002_JavaScript Algorithms-and-Data-Structures/08_Functional-Programming/05_pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -0,0 +1,12 @@
+// The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(value) {
+  return value + 1;
+ 
+  // Only change code above this line
+}
+
+var differentValue = incrementer(fixedValue); // Should equal 5
+console.log(fixedValue);
